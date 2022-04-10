228ba3a57d9eec17abc300c9d2ca59eea714f4db
IdeaBosque <ideabosque@github.com>
2022-04-10 14:06:38 -0700

    Updated __init__.py

--- datawald_dynamodbagency/__init__.py
+++ datawald_dynamodbagency/__init__.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+__author__ = "bibow"
+
 __all__ = ["datawald_dynamodbagency"]
 from .dynamodbagency import DynamoDBAgency
 from .dynamodbagent import DynamoDBAgent
